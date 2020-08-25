6792e7a3402b518f575b7e4f0279322a2fd066dd
Juansito_the_dev <tecnosebastube@gmail.com>
2020-08-24 21:59:28 -0500

    mas eventos para un boton nuevo

--- code.js
+++ code.js
@@ -1,9 +1,15 @@
 var p = document.getElementById('parrafo');
 var txt = document.getElementById('texto');
 var boton = document.getElementById('boton');
+var add = document.getElementById('añadir');
 
 boton.addEventListener('click', evento);
+add.addEventListener('click', añadir);
 
 function evento() {
     p.innerHTML = txt.value;
+}
+
+function añadir() {
+    p.innerHTML += txt.value;
 }
